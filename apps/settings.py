ca711648f299af53c3a74b9320d8e0f677e1bbcd
deadempty <[email>
2022-11-12 18:05:00 +0300

    Deploy to Heroku

--- apps/settings.py
+++ apps/settings.py
@@ -238,13 +238,13 @@ STATIC_URL = 'static/'
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
