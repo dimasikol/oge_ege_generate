effa321169d161ded8a4fd126a761438ecc06fab
deadempty <[email>
2022-11-12 19:04:21 +0300

    Deploy to Heroku

--- apps/settings.py
+++ apps/settings.py
@@ -235,13 +235,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 CKEDITOR_UPLOAD_PATH = "uploads/"
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "static"),
 # ]
--- chanelmessages/urls.py
+++ chanelmessages/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from .views import chat_view
 
-urlpatterns = []
-    # path('', chat_view.index, name='index'),
-    # path('<str:room_name>/', chat_view.room, name='room'),
+urlpatterns = [
+     path('', chat_view.index, name='index'),
+     path('<str:room_name>/', chat_view.room, name='room'),
+]
